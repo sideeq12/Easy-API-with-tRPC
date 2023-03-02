ef61b52a595bd8b1f3239c8efd42321401659d18
sideeq12 <abdwaheed2018@gmail.com>
2023-03-02 16:44:57 +0000

    trpc: adapter fixes

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,12 +1,30 @@
 import express from "express";
 import * as trpc from "@trpc/server"
+import * as trpcExpress from "@trpc/server/adapters/express"
 
-const appRoouter = trpc.router().query("Hello", {
+
+// Creating our router ; here we are creating a query route because we want to send(query) data to the client
+const appRouter = trpc.router().query("Hello", {
   resolve(){
     return "Hello world"
   }
-})
+}).interop()
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+
 const app = express();
+
+
+// Connecting our tRPC adapter to express
+app.use("/trpc", 
+trpcExpress.createExpressMiddleware({
+  router : appRouter,
+  createContext
+}))
+
+// declaring the port on which our server is running
 const port = 8080;
 
 app.get("/", (req, res) => {
